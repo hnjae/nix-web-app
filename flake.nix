b5acc420be69c1db5b5854340dad628cb7316cae
KIM Hyunjae <hnjae@outlook.com>
2024-08-18 00:59:28 +0900

    refactor: make option and packages to separate module

--- flake.nix
+++ flake.nix
@@ -2,6 +2,6 @@
   description = "Declarative web app manager for home-manager";
 
   outputs = {
-    homeManagerModules.default = import ./hm-module;
+    homeManagerModules.default = import ./modules/home-manager.nix;
   };
 }
