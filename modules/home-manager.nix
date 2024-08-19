0eb05ce0c53fde6df0aeb01c72b53fc12040c133
KIM Hyunjae <hnjae@outlook.com>
2024-08-19 14:23:16 +0900

    feat: remove web-app.enable option
    Remove the unnecessary enable flag as the presence of `cfg.apps` can
    determine the required condition.

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -8,7 +8,7 @@
 in {
   options.web-app = (import ./share/option.nix) {inherit lib pkgs;};
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf (cfg.apps != []) {
     home.packages = (import ./share/packages) {inherit cfg pkgs;};
   };
 }
